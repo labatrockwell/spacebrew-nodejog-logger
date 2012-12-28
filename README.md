caab21f60fe72dea4e3632b24895434f243ac2c6
julio terra <julioterra@gmail.com>
2012-12-28 02:00:33 -0500

    updated readme and cleaned up code.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Spacebrew Node Logger  
 =====================  
-This Spacebrew app logs all data that it receives via a boolean, a range, and a string channel. Logged data is saved in a text file. 
+This Spacebrew app logs all data that it receives via a boolean, a range, and a string channel. All data is saved in a text file in a local tmp directory. You can configure this app by passing arguments to it via the command line, as described below. 
   
 Installing Node and Dependencies  
 --------------------------------  
@@ -10,7 +10,10 @@ Before you can start up the logger you need to install node.js along with two no
 To install node.js use the packaged installer that is available on http://nodejs.org/. This package also includes node packaged modules (npm), a command-line utility that manages the installation of node modules.  
   
 ###2. Installing Node Modules  
-The node logger requires the websockets (ws) and jog logger (jog) modules. The easiest way to install these modules is by using node packaged modules via terminal/command line:  
+The node logger requires the websockets (ws) and jog logger (jog) modules. The easiest way to install these modules is by using node packaged modules.  
+* Open up the Terminal app (or another command-line app).   
+* Go to the base directory of the node logger app.  
+* Run the following commands  
   
 ```
 npm install ws  
@@ -19,10 +22,16 @@ npm install jog
   
 Running the Logger  
 ------------------  
-To start the logger just enter the following command:
+Here are the steps for s
+* Open up the Terminal app (or another command-line app).  
+* Go to the base directory of the node logger app.  
+* Run the start-up command with the appropriate arguments   
   
-```
-node spacelog.js  
-```
+```  
+node spacelog.js [optional arguments]  
+```  
   
-The log file will be saved in the following directory: /tmp/tail  
+Here is a list of the optional arguments that can be added to the app launch command. All optional arguments are structured as follows key=value:  
+* `server=server_name` server_name holds the hostname of the spacebrew server. If no server hostname is specified then app will attempt to connect to "localhost".  
+* `name=app_name` app_name holds the client app name that will be registered with spacebrew. If argument not provided then client app name will be set to "spacelog".  
+* `file=file_name` file_name holds the name of the file that where data will be saved. If no file name passed then file name is set to "tail". All data saved in "tmp" folder.  
--- spacelog.js
+++ spacelog.js
@@ -6,20 +6,42 @@
 var Jog = require('Jog');
 var WebSocket = require('ws');
 
-var fileName = "tail";
-var logId = 0;
+var data = {};
+data.file = "tail";
+data.id = 0;
 
 var sb = {};
-    sb.server = 'test';
-    sb.name = "spacelog";
-    sb.desc = "This will log incoming data to a file called /tmp/tail for later use.";
-    sb.config = {
+sb.server = '127.0.0.1';
+sb.name = "spacelog";
+sb.desc = "This will log incoming data to a file called /tmp/tail for later use.";
+
+if (process.argv.length > 2) {
+  for ( var i = 2; i < process.argv.length; i ++ ) {
+    var expression = /([-a-zA-Z]+)=([-a-zA-Z0-9\.]+)/;
+    var m = process.argv[i].match(expression);
+    if (m) {
+      if (m[1] == "server") { 
+        sb.server = m[2];
+        console.log("set server hostname: " + sb.server);
+      }
+      else if (m[1] == "name") {
+        sb.name = m[2];
+        console.log("set app name: " + sb.name);
+      }
+      else if (m[1] == "file") {
+        data.file = m[2];
+        console.log("set file name: " + data.file);
+      }
+    }
+  }
+}
+
+sb.config = {
   "config": {
     "name": sb.name,
     "description": sb.desc,
     "publish": {
-      "messages": [
-      ]
+      "messages": []
     },
     "subscribe": {
       "messages": [
@@ -40,30 +62,10 @@ var sb = {};
   }
 };
 
-if (process.argv.length > 2) {
-  for ( var i = 2; i < process.argv.length; i ++ ) {
-    var expression = /([-a-zA-Z]+)=([-a-zA-Z0-9\.]+)/;
-    var m = process.argv[i].match(expression);
-    if (m) {
-      if (m[1] == "server") { 
-        sb.server = m[2];
-        console.log("set server hostname: " + sb.server);
-      }
-      else if (m[1] == "name") {
-        sb.name = m[2];
-        console.log("set app name: " + sb.name);
-      }
-      else if (m[1] == "file") {
-        fileName = m[2];
-        console.log("set file name: " + fileName);
-      }
-    }
-  }
-}
+data.path = require('path').resolve(__dirname, ('tmp/' + data.file + '.dat'));
+data.log = Jog(new Jog.FileStore(data.path)); 
 
-var filepath = path.resolve(__dirname, (fileName + '.dat'));
-var log = Jog(new Jog.FileStore(filepath)); 
-    sb.conn = new WebSocket("ws://"+sb.server+":9000");  
+sb.conn = new WebSocket("ws://"+sb.server+":9000");  
 
 sb.conn.onopen = function() {
   console.log("WebSockets connection opened");
@@ -77,7 +79,7 @@ sb.conn.onmessage = function(e) {
     var tName = tMsg.message.name;
     var tType = tMsg.message.type;
     var tValue = tMsg.message.value;
-    log.info( { logId: ++logId, user: sb.name, name: tName, sbtype: tType, value: tValue } );
+    data.log.info( { data: ++data.id, user: sb.name, name: tName, sbtype: tType, value: tValue } );
 
 }
 
@@ -90,15 +92,16 @@ sb.conn.onerror = function(e) {
 
 }
 
+// When the "error" event is emitted for the spacebrew connection, this is called
+sb.conn.on("error", function(error) {
+  console.log("+++++++ ERROR +++++++");
+  console.error(error);
+});
+
 // tail the json "documents"
-log.stream({ end: false, interval: 500 })
+data.log.stream({ end: false, interval: 500 })
   .on('data', function(obj){
     console.log(obj);
   });
 
-sb.conn.on("error", function(error) {
-  // When the "error" event is emitted, this is called
-  console.log("+++++++ ERROR +++++++");
-  console.error(error);
-});
 
