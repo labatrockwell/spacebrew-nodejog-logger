62a68d5d5a0ec4307cf7ec2c219491b55b7d092f
julio terra <julioterra@gmail.com>
2012-12-27 23:25:28 -0500

    updated readme, cleaned up code.

--- README.md
+++ README.md
@@ -1,27 +1,28 @@
 Spacebrew Node Logger  
 =====================  
+This Spacebrew app logs all data that it receives via a boolean, a range, and a string channel. Logged data is saved in a text file. 
   
-
-Installing Dependencies
------------------------
-Before you can start up the logger you need to install node.js along with two node modules. If you already have node.js installed, then you just need to install the required modules.
-
-###1. Installing Node.js
-To install node.js use the packaged installer that is available on http://nodejs.org/. This package also includes node packaged modules (npm), a command-line utility that manages the installation of node modules.
-
-###2. Installing Node Modules
-The easiest way to install the two required modules is through node packaged modules.   
-
+Installing Node and Dependencies  
+--------------------------------  
+Before you can start up the logger you need to install node.js along with two node modules. If you already have node.js installed, then you just need to install the required modules.  
+  
+###1. Installing Node.js  
+To install node.js use the packaged installer that is available on http://nodejs.org/. This package also includes node packaged modules (npm), a command-line utility that manages the installation of node modules.  
+  
+###2. Installing Node Modules  
+The node logger requires the websockets (ws) and jog logger (jog) modules. The easiest way to install these modules is by using node packaged modules via terminal/command line:  
+  
 ```
-$ npm install ws  
-$ npm install jog  
+npm install ws  
+npm install jog  
 ```
-
+  
 Running the Logger  
 ------------------  
+To start the logger just enter the following command:
   
 ```
-$ node spacelog.js  
+node spacelog.js  
 ```
-
-It writes files to /tmp/tail
+  
+The log file will be saved in the following directory: /tmp/tail  
--- spacelog.js
+++ spacelog.js
@@ -3,50 +3,57 @@
 // log it
 // and forward it along
 
+var Jog = require('Jog');
+var log = Jog(new Jog.FileStore('/tmp/tail')); 
+var id = 0;
+
 var WebSocket = require('ws');
-var server = '127.0.0.1';
+var server = '10.0.1.11';
+// var ws = {};
 var ws = new WebSocket("ws://"+server+":9000");
 
+var sbName = "spacelog";
 
 var myConfig = {
-"config": {
- "name": "spacelogger",
- "description": "This will log incoming data to a file called /tmp/tail for later use.",
- "publish": {
-   "messages": [
-   ]
- },
- "subscribe": {
-   "messages": [
-     {
-       "name": "booleanData",
-       "type": "boolean"
-     },
-     {
-       "name": "rangeData",
-       "type": "range"
-     },
-     {
-       "name": "stringData",
-       "type": "string"
-     }
-   ]
- }
-}
+  "config": {
+    "name": sbName,
+    "description": "This will log incoming data to a file called /tmp/tail for later use.",
+    "publish": {
+      "messages": [
+      ]
+    },
+    "subscribe": {
+      "messages": [
+        {
+          "name": "booleanData",
+          "type": "boolean"
+        },
+        {
+          "name": "rangeData",
+          "type": "range"
+        },
+        {
+          "name": "stringData",
+          "type": "string"
+        }
+      ]
+    }
+  }
 };
 
 ws.onopen = function() {
-    console.log("WebSockets connection opened");
+  console.log("WebSockets connection opened");
   // send my config
   ws.send(JSON.stringify(myConfig));
 }
+
 ws.onmessage = function(e) {
     console.log("Got WebSockets message: " + e.data);
     var tMsg = JSON.parse(e.data);
     var tName = tMsg.message.name;
     var tType = tMsg.message.type;
     var tValue = tMsg.message.value;
-    log.info(e.data, { id: ++id, user: 'spacelogger', name: tName, sbtype: tType, value: tValue });
+    log.info( { id: ++id, user: sbName, name: tName, sbtype: tType, value: tValue } );
 
 }
 
@@ -54,11 +61,10 @@ ws.onclose = function() {
     console.log("WebSockets connection closed");
 }
 
+ws.onerror = function(e) {
+  console.log("onerror ", e);
 
-var jog = require('jog')
-  , log = jog(new jog.FileStore('/tmp/tail'))
-  , id = 0;
-
+}
 
 // tail the json "documents"
 log.stream({ end: false, interval: 500 })
